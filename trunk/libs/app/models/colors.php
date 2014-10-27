35e12c35ea06d4809b6d93776f501f3560a47233
brunovschettini@gmail.com <brunovschettini@gmail.com@8d0f86d9-64fb-3fd2-15b0-51bdbd2efda4>
2014-10-27 13:38:35 +0000

    .

---
+++ trunk/libs/app/models/colors.php
@@ -0,0 +1,60 @@
+<?php
+
+//
+
+/**
+ * SweetPHP <br />
+ * <p><b>colors</b></p>
+ * @package SweetPHP
+ * @package Apps/Model/colors
+ * @author sweetphp
+ */
+
+/** @Table("colors") */
+class colors extends Model {
+
+    /** @Column("{'name':'id', 'type':'bigint','length':'20', 'primaryKey':'true'}") */
+    public $id;
+    
+    /** @Column("{'name':'name', 'type':'varchar','length':'50', 'nullable':'false'}") */
+    public $name;
+	
+	/** @JoinColumn("{'name':'languages', 'referencedColumnName':'id'"}) */
+    public $languages;	
+
+    public function __construct() {
+        $this->id = null;
+		$this->name = $name;
+		$this->languages = $languages;
+        parent::__construct();
+    }
+
+    public function construct($id = null, $name = "") {
+        $this->id = $id;
+		$this->name = $name;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($var = 0) {
+        return $this->id = $var;
+    }
+	
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($var = "") {
+        return $this->name = $var;
+    }
+	
+    public function getLanguages() {
+        return $this->languages;
+    }
+
+    public function setLanguages(languages $var = null) {
+        return $this->languages = $var;
+    }	
+}

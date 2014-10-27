b447f15cb38142a889d72f0697965a6a46aec26a
brunovschettini@gmail.com <brunovschettini@gmail.com@8d0f86d9-64fb-3fd2-15b0-51bdbd2efda4>
2014-10-27 13:48:57 +0000

    .

---
+++ trunk/libs/app/models/type_notify.php
@@ -0,0 +1,26 @@
+<?php
+
+//
+
+/**
+ * SweetPHP <br />
+ * <p><b>type_notify</b></p>
+ * @package SweetPHP
+ * @package Apps/Model/type_notify
+ * @author sweetphp
+ */
+
+/** @Table("type_notify") */
+class type_notifyextends Model {
+
+    /** @Column("{'name':'id', 'type':'bigint','length':'20', 'primaryKey':'true'}") */
+    public $id;
+    
+    /** @Column("{'name':'name', 'type':'varchar','length':'255', 'nullable':'false'}") */
+    public $name;
+	
+	/** @JoinColumn("{'name':'languages', 'referencedColumnName':'id'"}) */
+    public $languages; 
+   
+}
+
